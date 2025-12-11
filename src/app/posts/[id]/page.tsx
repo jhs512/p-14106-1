d323144fe0765d233711d9020dc07aaeaf24e06d
jhs512 <jangka512@gmail.com>
2025-12-11 14:35:01 +0900

    c30

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,10 +1,10 @@
 "use client";
 
 import { apiFetch } from "@/lib/backend/client";
-import type { PostWithContentDto } from "@/type/post";
-import { useEffect, useState } from "react";
-import { useParams, useRouter } from "next/navigation";
+import type { PostCommentDto, PostWithContentDto } from "@/type/post";
 import Link from "next/link";
+import { useParams, useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function Page() {
   const { id: idStr } = useParams<{ id: string }>();
@@ -12,6 +12,7 @@ export default function Page() {
   const router = useRouter();
 
   const [post, setPost] = useState<PostWithContentDto | null>(null);
+  const [postComments, setPostComments] = useState<PostCommentDto[] | null>(null);
 
   const deletePost = (id: number) => {
     apiFetch(`/api/v1/posts/${id}`, {
@@ -23,8 +24,9 @@ export default function Page() {
   };
 
   useEffect(() => {
-    apiFetch(`/api/v1/posts/${id}`)
-      .then(setPost);
+    apiFetch(`/api/v1/posts/${id}`).then(setPost);
+
+    apiFetch(`/api/v1/posts/${id}/comments`).then(setPostComments);
   }, []);
 
   if (post == null) return <div>로딩중...</div>;
@@ -40,7 +42,10 @@ export default function Page() {
       <div className="flex gap-2">
         <button
           className="p-2 rounded border"
-          onClick={() => confirm(`${post.id}번 글을 정말로 삭제하시겠습니까?`) && deletePost(post.id)}
+          onClick={() =>
+            confirm(`${post.id}번 글을 정말로 삭제하시겠습니까?`) &&
+            deletePost(post.id)
+          }
         >
           삭제
         </button>
@@ -48,6 +53,22 @@ export default function Page() {
           수정
         </Link>
       </div>
+
+      <h2>댓글 목록</h2>
+
+      {postComments == null && <div>댓글 로딩중...</div>}
+
+      {postComments != null && postComments.length == 0 && (
+        <div>댓글이 없습니다.</div>
+      )}
+
+      {postComments != null && postComments.length > 0 && (
+        <ul>
+          {postComments.map((comment) => (
+            <li key={comment.id}>{comment.content}</li>
+          ))}
+        </ul>
+      )}
     </>
   );
 }
