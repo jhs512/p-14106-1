a6fcfbfcdbf786a8898b831e063ff6a543f07102
jhs512 <jangka512@gmail.com>
2025-12-12 10:43:31 +0900

    c37-2

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -6,6 +6,42 @@ import Link from "next/link";
 import { useParams, useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
+function PostInfo({ post }: { post: PostWithContentDto }) {
+  const router = useRouter();
+
+  const deletePost = (id: number) => {
+    apiFetch(`/api/v1/posts/${id}`, {
+      method: "DELETE",
+    }).then((data) => {
+      alert(data.msg);
+      router.replace("/posts");
+    });
+  };
+
+  return (
+    <>
+      <div>번호 : {post.id}</div>
+      <div>제목: {post.title}</div>
+      <div style={{ whiteSpace: "pre-line" }}>{post.content}</div>
+
+      <div className="flex gap-2">
+        <button
+          className="p-2 rounded border"
+          onClick={() =>
+            confirm(`${post.id}번 글을 정말로 삭제하시겠습니까?`) &&
+            deletePost(post.id)
+          }
+        >
+          삭제
+        </button>
+        <Link className="p-2 rounded border" href={`/posts/${post.id}/edit`}>
+          수정
+        </Link>
+      </div>
+    </>
+  );
+}
+
 function PostCommentWriteAndList({
   id,
   postComments,
@@ -117,21 +153,11 @@ function PostCommentWriteAndList({
 export default function Page() {
   const { id: idStr } = useParams<{ id: string }>();
   const id = Number(idStr);
-  const router = useRouter();
 
   const [post, setPost] = useState<PostWithContentDto | null>(null);
   const [postComments, setPostComments] = useState<PostCommentDto[] | null>(
     null
   );
-
-  const deletePost = (id: number) => {
-    apiFetch(`/api/v1/posts/${id}`, {
-      method: "DELETE",
-    }).then((data) => {
-      alert(data.msg);
-      router.replace("/posts");
-    });
-  };
 
   useEffect(() => {
     apiFetch(`/api/v1/posts/${id}`)
@@ -153,24 +179,7 @@ export default function Page() {
     <>
       <h1>글 상세페이지</h1>
 
-      <div>번호 : {post.id}</div>
-      <div>제목: {post.title}</div>
-      <div style={{ whiteSpace: "pre-line" }}>{post.content}</div>
-
-      <div className="flex gap-2">
-        <button
-          className="p-2 rounded border"
-          onClick={() =>
-            confirm(`${post.id}번 글을 정말로 삭제하시겠습니까?`) &&
-            deletePost(post.id)
-          }
-        >
-          삭제
-        </button>
-        <Link className="p-2 rounded border" href={`/posts/${post.id}/edit`}>
-          수정
-        </Link>
-      </div>
+      <PostInfo post={post} />
 
       <PostCommentWriteAndList
         id={id}
